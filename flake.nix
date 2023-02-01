1750637dd4218bf63e4db74ee572a861a7faa938
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-02-01 17:15:21 +0100

    chore: finish setting up environement

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
+let
+  rustVersion = "1.61.0";
+  cargo2nixVersion = "release-0.11.0";
+in
 {
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/${cargo2nixVersion}";
     flake-utils.follows = "cargo2nix/flake-utils";
     nixpkgs.follows = "cargo2nix/nixpkgs";
   };
@@ -14,7 +18,7 @@
         };
 
         rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.61.0";
+          rustVersion = "${rustVersion}";
           packageFun = import ./Cargo.nix;
         };
 
