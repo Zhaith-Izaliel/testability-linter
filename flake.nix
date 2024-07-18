3ed3111e17257cde1dd6eef29825cfeffd8844ed
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-07-18 03:00:58 +0200

    build(flake): update build system to modern standards

--- flake.nix
+++ flake.nix
@@ -1,93 +1,34 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
-  outputs = inputs: with inputs;
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [cargo2nix.overlays.default];
-      };
+  outputs = inputs @ {flake-parts, ...}: let
+    inherit (cargoToml.package) name version;
+    cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} ({withSystem, ...}: {
+      systems = ["x86_64-linux" "aarch64-darwin" "x86_64-darwin"];
 
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells = {
+          # nix develop
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              rustc
+              cargo
+              rust-analyzer
+            ];
+          };
+        };
 
-      # 2. Builds the rust package set, which contains all crates in your cargo workspace's dependency graph.
-      # `makePackageSet` accepts the following arguments:
-      # - `packageFun` (required): The generated `Cargo.nix` file, which returns the whole dependency graph.
-      # - `workspaceSrc` (optional): Sources for the workspace can be provided or default to the current directory.
-      # You must set some combination of `rustChannel` + `rustVersion` or `rustToolchain`.
-      # - `rustToolchain` (optional): Completely override the toolchain.  Must provide rustc, cargo, rust-std, and rust-src components
-      # - `rustChannel` (optional): "nightly" "stable" "beta".  To support legacy use, this can be a version when supplied alone.  If unspecified, defaults to "stable".
-      # - `rustVersion` (optional): "1.60.0" "2020-12-30".  If not supplied, "latest" will be assumed.
-      # - `rustProfile` (optional): "minimal" or "default" usually.  "minimal" if not specified (for faster builds)
-      # - `extraRustComponents` (optional): ["rustfmt" "clippy"].
-      # - `packageOverrides` (optional):
-      #     A function taking a package set and returning a list of overrides.
-      #     Overrides are introduced to provide native inputs to build the crates generated in `Cargo.nix`.
-      #     See `overlay/lib/overrides.nix` on how to create overrides and `overlay/overrides.nix` for a list of predefined overrides.
-      #     Most of the time, you can just use `overrides.all`. You can hand-pick overrides later if your build becomes too slow.
-      # - `rootFeatures` (optional):
-      #     A list of activated features on your workspace's crates.
-      #     Each feature should be of the form `<crate_name>[/<feature>]`.
-      #     If `/<feature>` is omitted, the crate is activated with no default features.
-      #     The default behavior is to activate all crates with default features.
-      # - `fetchCrateAlternativeRegistry` (optional): A fetcher for crates on alternative registries.
-      # - `release` (optional): Whether to enable release mode (equivalent to `cargo build --release`), defaults to `true`.
-      # - `hostPlatformCpu` (optional):
-      #     Equivalent to rust's target-cpu codegen option. If specified "-Ctarget-cpu=<value>" will be added to the set of rust
-      #     flags used for compilation of the package set.
-      # - `hostPlatformFeatures` (optional):
-      #     Equivalent to rust's target-feature codegen option. If specified "-Ctarget-feature=<values>" will be added to the set of rust
-      #     flags used for compilation of the package set. The value should be a list of the features to be turned on, without the leading "+",
-      #     e.g. `[ "aes" "sse2" "ssse3" "sse4.1" ]`.  They will be prefixed with a "+", and comma delimited before passing through to rust.
-      #     Crates that check for CPU features such as the `aes` crate will be evaluated against this argument.
-      #    rustcLinkFlags (optional):
-      #     Pass extra flags directly to rustc during non-build invocations
-      #    rustcBuildFlags (optional):
-      #     Pass extra flags directly to Rustc during build invocations
-      # - `target` (optional):
-      #     Set an explicit Rust output target.  Overrides the translation
-      #     from Nix targets to Rust targets.  See overlay/lib/rust-triple.nix
-      #     for more info.
-      rustPkgs = pkgs.rustBuilder.makePackageSet {
-        rustVersion = "1.61.0";
-        packageFun = import ./Cargo.nix;
+        packages = {
+          default = pkgs.callPackage ./nix {inherit version name;};
+        };
       };
-
-      cargo2nixBin = cargo2nix.packages.${system}.default;
-
-      workspaceShell = (rustPkgs.workspaceShell {
-        packages = [
-          pkgs.rustup
-          pkgs.rust-analyzer
-          cargo2nixBin
-          pkgs.jdk
-          pkgs.cargo-tarpaulin
-        ];
-        # shellHook = ''
-        #   echo "In shell"
-        # '';
-      }); # supports override & overrideAttrs
-    in rec {
-      devShells = {
-        # nix develop
-        "${system}".default = workspaceShell;
-        default = workspaceShell;
-      };
-      packages = {
-        testability-linter = (rustPkgs.workspace.testability-linter {}).bin;
-        default = packages.testability-linter;
-        shell = devShells.default;
-      };
-    }
-  );
+    });
 }
-
